efc9f3e42c2f438243f6a6803324011587e24b8f
khodorsinno <khodorsinno594@gmail.com>
2026-07-11 00:20:05 +0300

    Add password check and heart animation functions

---
+++ script.js
@@ -0,0 +1,45 @@
+const secretCode = "1234";
+
+function checkPassword(){
+
+let password = document.getElementById("password").value;
+
+if(password===secretCode){
+
+window.location.href="home.html";
+
+}
+
+else{
+
+document.getElementById("error").innerHTML="Wrong Password ❤️";
+
+}
+
+}
+
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML="❤";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.animationDuration=(Math.random()*4+4)+"s";
+
+heart.style.fontSize=(Math.random()*20+15)+"px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},8000);
+
+}
+
+setInterval(createHeart,300);
