17e8924f8e56cac053ecc5662aa24ee49142f543
khodorsinno <khodorsinno594@gmail.com>
2026-07-11 03:05:41 +0300

    Change secret code and add heart creation function
    Updated the secret code and added a function to create hearts on the screen.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const secretCode = "1234";
+const secretCode = "20/02/2026";
 
 function checkPassword(){
 
@@ -18,6 +18,31 @@ document.getElementById("error").innerHTML="Wrong Password ❤️";
 
 }
 
+function createHeart(){
+
+const heart=document.createElement("div");
+
+heart.classList.add("heart");
+
+heart.innerHTML="❤";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.animationDuration=(Math.random()*4+4)+"s";
+
+heart.style.fontSize=(Math.random()*20+15)+"px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},8000);
+
+}
+
+setInterval(createHeart,300);
 function createHeart(){
 
 const heart=document.createElement("div");
